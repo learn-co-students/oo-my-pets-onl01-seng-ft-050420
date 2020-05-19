37b1269a95102c744a9ebb94bf9ebf3bcb3f469b
kiku ishimoto <kikuish@gmail.com>
2020-05-19 18:39:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+  attr_reader :name
+  attr_accessor :owner, :mood
+  @@all = []
   # code goes here
+  def initialize(name, owner, mood = 'nervous')
+    @name = name
+    @owner = owner
+    @mood = mood
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+
 end
